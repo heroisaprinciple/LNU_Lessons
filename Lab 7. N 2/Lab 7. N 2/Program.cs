1aae1366dee71423e2bfc1d45434684abb6667cc
heroisaprinciple <71873660+heroisaprinciple@users.noreply.github.com>
2020-12-11 01:56:37 +0200

    Add files via upload

---
+++ Lab 7. N 2/Lab 7. N 2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab_7._N_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
